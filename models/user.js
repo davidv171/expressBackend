e84df5acdf4a72df52abbd39d4df1d768c652de0
David <david.vuckovic7@gmail.com>
2018-08-11 15:31:19 +0200

    Got rid of the linter

--- models/user.js
+++ models/user.js
@@ -1,58 +1,47 @@
-const bcrypt = require("bcrypt");
 module.exports=(sequelize,DataTypes) =>{
-    //TODO Create explicit migration
-    const user = sequelize.define("user",
-		{
-			id:
+    const user = sequelize.define('user',
+        {
+            id:
             {
-            	type: DataTypes.INTEGER,
-            	primaryKey: true,
-            	unique: true,
-            	autoIncrement: true,
-            	required: true
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                unique: true,
+                autoIncrement: true,
+                required: true
             },
-			username:
+            createdAt:false,
+            username:
             {
-            	type: DataTypes.STRING,
-            	unique: true,
-            	required:true,
-                validate:
-                {
-                    len:[4,32]
-                }
+                type: DataTypes.STRING,
+                unique: true,
+                required:true
             },
-			password:{
-				type: DataTypes.STRING,
-				required:true,
-				protect:true,
-                validate:
-                {
-                    len:[4,32]
-                }
-
+            password:{
+                type: DataTypes.STRING,
+                required:true,
+                protect:true
                 
-			},
-			likedByCount:
+            },
+            likedByCount:
             {
-            	type: DataTypes.INTEGER,
-            	defaultValue:0
+                type: DataTypes.INTEGER,
+                defaultsTo:0
             }
-		}
-    
-    );
-	user.associate = function(models)
-	{
-		models.user.hasMany(models.like,
-			{
-				onDelete:"cascade"
-			});
-	};
-    //TODO: Add a validate that checks username and password length and first character(must not be a number)
-    
-	user.beforeCreate((user) => {
+        });i
+    user.associate = function(models)
+    {
+        models.user.hasMany(models.like,
+        {
+            onDelete:'cascade'
+        });
+    };
+    	user.beforeCreate((user) => {
 		return bcrypt.hash(user.password,10).then(hashedPw => {
 			user.password = hashedPw;
 		});
 	});
-	return user;
+
+    return user;
+        
+    
 };
