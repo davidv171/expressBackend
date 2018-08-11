39901677b5ab6408539bf39b1591d42cae503786
David <david.vuckovic7@gmail.com>
2018-08-11 15:39:38 +0200

    Re-added ESLint, removed unnecessary lines in index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express');
-const Sequelize = require('sequelize');
 var bodyParser = require('body-parser');
 var routes = require('./routes/routes.js');
 var app = express();
-const database = new Sequelize("postgres://popmmtnk:k4p7M5zyk6VhgC_pr8PimC1PFQ4l_nxp@horton.elephantsql.com:5432/popmmtnk");
 //Lets us parse request bodies(important for POST)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
